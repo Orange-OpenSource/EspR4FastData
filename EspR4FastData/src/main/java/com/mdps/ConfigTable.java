97b415286aef19a184657d08693d78559842eed8
Laurent ARTUSIO <laurent.artusio@orange.com>
2014-10-30 13:56:40 +0100

    ert

--- EspR4FastData/src/main/java/com/mdps/ConfigTable.java
+++ EspR4FastData/src/main/java/com/mdps/ConfigTable.java
@@ -3,7 +3,7 @@
 This software is distributed under the terms and conditions of the 
 'GNU GENERAL PUBLIC LICENSE Version 2' license which can be found 
 in the file 'LICENSE.txt' in this package distribution or at 
-'http://www.gnu.org/licenses/gpl-2.0-standalone.html'. 
+'http://www.gnu.org/licenses/gpl-2.0-standalone.html'.
 */
 
 package com.mdps;
